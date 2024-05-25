9cf65141434c449ad9e21863fb46f01bc53946b5
Shariful4447 <shariful.islam4447@gmail.com>
2024-05-25 22:12:21 +0200

    config with moongose complete

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import { MovieRoute } from "./modules/movies/movie.routes";
+import { productRoute } from "./modules/product/product.route";
+import { orderRoute } from "./modules/order/order.route";
+const app = express();
+const port = 3000;
+
+//parser json
+app.use(express.json());
+app.use("/api/movies", MovieRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
+export default app;
